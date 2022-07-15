c19d10e08e0439fbc5e873bfb5555dc83c74fab8
zs-zc <3011121187@qq.com>
2022-07-15 17:10:23 +0800

    fix:路由重定向

--- 路由使用及封装/App.jsx
+++ 路由使用及封装/App.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
-import { Switch, NavLink, Route, Redirect } from "react-router-dom"
+import { Switch, Route, } from "react-router-dom"
 import About from "./pages/About"
 import Home from './pages/Home'
+import MyNavLink from './components/MyNavLink'
 export default class App extends Component {
     render() {
         return (
             <div>
                 <h2>router 跳转</h2>
                 <div >
-                    <NavLink to="/home" >Home</NavLink>
-                    <NavLink to="/about" style={{ marginLeft: '10px' }}>About</NavLink>
+                    <MyNavLink to="/home" >Home</MyNavLink >
+                    <MyNavLink to="/about" style={{ marginLeft: '10px' }}>About</MyNavLink>
                 </div>
                 <div>
                     <Switch>
                         <Route path="/about" component={About} />
                         <Route path="/home" component={Home} />
-                        <Redirect to='/home' />
                         {/* xxx  为演示，组件已删除 */}
                     </Switch>
                 </div>
