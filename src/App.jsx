58b65097fd0c6cf880a17c36652fed3c3e223f9f
zs-zc <3011121187@qq.com>
2022-07-15 15:38:40 +0800

    fix:路由以及对路由的二次封装

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Route, } from "react-router-dom"
+import About from "./pages/About"
+import Home from './pages/Home'
+import MyNavLink from './components/MyNavLink'
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>router 跳转</h2>
+                <div>
+                    <MyNavLink to="/home" >Home</MyNavLink>
+                    <br />
+                    <MyNavLink to="/about" >About</MyNavLink>
+                </div>
+                <div>
+                    <Route path="/about" component={About} />
+                    <Route path="/home" component={Home} />
+                </div>
+            </div>
+
+        )
+    }
+}
