d96472ffa6e12c2ee21e1c1541685a153a048fce
zs-zc <3011121187@qq.com>
2022-07-15 15:57:37 +0800

    fix:Switch包裹后只匹配相同路径的一个路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, } from "react-router-dom"
+import { Switch, Route, } from "react-router-dom"
 import About from "./pages/About"
 import Home from './pages/Home'
 import MyNavLink from './components/MyNavLink'
@@ -13,8 +13,11 @@ export default class App extends Component {
                     <MyNavLink to="/about" style={{ marginLeft: '10px' }}>About</MyNavLink>
                 </div>
                 <div>
-                    <Route path="/about" component={About} />
-                    <Route path="/home" component={Home} />
+                    <Switch>
+                        <Route path="/about" component={About} />
+                        <Route path="/home" component={Home} />
+                        {/* xxx  为演示，组件已删除 */}
+                    </Switch>
                 </div>
             </div >
 
